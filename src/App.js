18651a2d6502e7675e2758f502587cb2dcb87b62
Cameron Ryan <camrya2004@gmail.com>
2025-01-17 09:57:55 -0500

    commit 1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div className="col-md-4">
+        <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/640px-Grosser_Panda.JPG"
+              className="card-img-top"
+              alt="Project Thumbnail"
+            />
+      </div>
+    </div>
+  );
+}
+
+export default App;
